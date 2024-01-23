507d8b45633a2061dabba2e9d0ec66c0277cacb8
Michael D. Marchionna <michael.marchionna@gmail.com>
2024-01-23 14:52:04 -0800

    Fix -noconfirm for build-macos.sh

--- build-macos.sh
+++ build-macos.sh
@@ -2,15 +2,16 @@
 #
 # PyInstaller: https://pyinstaller.org/en/stable/index.html
 # ImageToIcons:
-#   https://apps.apple.com/us/app/image2icon-make-your-icons/id992115977?mt=12
+s#   https://apps.apple.com/us/app/image2icon-make-your-icons/id992115977?mt=12
 #
 set -x
 
 if [ -e PWVault.spec ]; then
-  pyinstaller PWVault.spec
+  pyinstaller --noconfirm PWVault.spec
+
 else
-    pyinstaller --windowed \
-                -y \
-                --osx-bundle-identifier com.midoma.pyapps.pwvault pwvuiapp.py \
-                --name PWVault
+  pyinstaller --windowed \
+              --noconfirm \
+              --osx-bundle-identifier com.midoma.pyapps.pwvault pwvuiapp.py \
+              --name PWVault
 fi
